fab8fef9dc95e1da362a37611f91ce70be82594b
Animesh Karne <127398605+animesh3640@users.noreply.github.com>
2023-09-11 16:51:31 +0000

    add static keyword

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,20 +1,20 @@
 package com.driver;
 
 public class Main {
-    public static void main(str arr[]){
+    public static void main(str[] args){
         Product p = new Product();
         int ans = p.product(10,20);
         int ans2 = p.product(10,20,30);
         double ans3 = p.product(10,20);
     }
     public class Product{
-        public int product(int x, int y) {
+        public static int product(int x, int y) {
             return x*y;
         }
-        public int product(int x, int y, int z) {
+        public static int product(int x, int y, int z) {
             return x*y*z;
         }
-        public double product(double x, double y) {
+        public static double product(double x, double y) {
             return x*y;
         }
     }
